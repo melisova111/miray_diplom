eb2eb43453803648cc4b73071aa168c071276b45
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-28 21:17:10 +0600

    changes links and import

--- src/Components/CategoryList/CategoryList.js
+++ src/Components/CategoryList/CategoryList.js
@@ -1,32 +1,31 @@
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import "./CategoryList.css";
-import { useContext } from "react";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
 import DeleteCategory from "../DeleteCategory/DeleteCategory";
-
+import "./CategoryList.css";
 
 export default function CategoryList() {
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
   const { categories } = useContext(AppContext);
 
   const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={"/categories/" + category.slug}>
-        {category.name}
+      <NavLink to={`/categories/${category.slug}`} onClick={scrollToTop}>
+    
+        <span>{category.name}</span>
       </NavLink>
 
       <DeleteCategory category={category} />
     </li>
-  ))
+  ));
 
-
-  
   return (
-    <div className="CategoryList">
-      <button className="dropdownBtn"><span>Categories </span><i className="fa-solid fa-chevron-down fa-xs"></i></button>
-      <ul className="dropdownContent">{output}
+    <div className="CategoryList flex">
+      {output}
       <AddCategory />
-      </ul>
     </div>
-  )
+  );
 }
