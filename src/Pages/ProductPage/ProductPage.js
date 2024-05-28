b438c3d708c1771609415c8176854c66ee924924
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-28 22:01:46 +0600

    created new productpage

---
+++ src/Pages/ProductPage/ProductPage.js
@@ -0,0 +1,10 @@
+import CategoryList from "../../Components/CategoryList/CategoryList";
+
+export default function ProductPage() {
+    return (
+        <div>
+            <h1>Product Page</h1>
+            <CategoryList />
+        </div>
+    )
+}
