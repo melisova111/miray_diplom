d47abc612ce48c19d683e4d8745ca095f7aa419c
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-28 15:52:14 +0600

    delete the links and added buttons and svg

--- src/Components/CartList/CartList.js
+++ src/Components/CartList/CartList.js
@@ -1,7 +1,7 @@
-import "./CartList.css";
+import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import "./CartList.css";
 
 export default function CartList() {
   const { products, cart, setCart } = useContext(AppContext);
@@ -9,11 +9,11 @@ export default function CartList() {
   function onQuantityChange(product, qty) {
     setCart({
       ...cart,
-      [product.id]: qty
-    })
+      [product.id]: qty,
+    });
   }
 
-  function onItemRemove(product) {
+  function onRemoveClick(product) {
     const newCart = { ...cart };
     delete newCart[product.id];
     setCart(newCart);
@@ -22,29 +22,40 @@ export default function CartList() {
   const productIds = Object.keys(cart);
 
   const output = products
-    .filter((product) => productIds.includes(product.id))
-    .map((product) => (
-      <div className="cartItem" key={product.id}>
-        <Link to={"/products/" + product.slug}>
-          <img src={product.picture} alt={product.name} />
-        </Link>
-        <Link to={"/products/" + product.slug}>{product.name}</Link>
-
-        <div className="inputs">
+    .filter(product => productIds.includes(product.id))
+    .map(product => (
+      <div className="CartItem" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={`/products/` + product.slug}> {product.name}</Link>
         <input
           type="number"
-          value={cart[product.name]}
+          value={cart[product.id]}
           min={1}
           onChange={(event) => onQuantityChange(product, +event.target.value)} />
-        <span>${(cart[product.id] * product.price).toFixed(2)}</span>
-        <i className="fa-solid fa-xmark" onClick={() => onItemRemove(product)} />
-          </div>
+        <span>{cart[product.id] * product.price} $</span>
+        <button
+          onClick={() => onRemoveClick(product)}
+          className="btn_cart"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+        </button>
       </div>
     ));
 
+  const total = products
+    .filter(product => productIds.includes(product.id))
+    .reduce((sum, product) => sum + (cart[product.id] * product.price), 0);
+
   return (
     <div className="CartList">
+      {/* <h1>Cart</h1> */}
       {output}
+      <div className="CartTotal">
+        <h2>Total: {total} $</h2>
+      </div>
     </div>
   )
 }
