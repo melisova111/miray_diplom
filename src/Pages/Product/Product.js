218a0566a4e6653d8459284da1abd7ed8bdc654a
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-28 22:01:28 +0600

    delete and so added new imports

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,259 +1,29 @@
-import './Product.css';
-import { Link } from 'react-router-dom';
-import CategoryList from '../../Components/CategoryList/CategoryList';
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { useMatch } from "react-router";
+import NotFound from "../NotFound/NotFound";
 
-function Product() {
-	return (
-		<>
-		<aside>
-            <CategoryList />
-          </aside>
+export default function Product() {
+  const { products } = useContext(AppContext);
+  const { params } = useMatch("/products/:slug");
 
-		<div className="pages_cards flex">
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/KDxXqJlEdro/rs:fit:440:0:1/q:100/plain/images/products/1/3803/859721435/large_Frame__29_.png@webp' alt='' className="pages_img" />
-					</Link>
+  const product = products.find(product => product.slug === params.slug);
 
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/JVjp0Zma7E0/rs:fit:440:0:1/q:100/plain/images/products/1/3804/859721436/Frame__30_.png@webp' alt='' className="pages_img" />
-					</Link>
+  if (!product) {
+    return <NotFound />
+  }
 
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/images/products/1/3811/859721443/Frame__53_.png' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/Z9lP4TMh2NY/rs:fit:440:0:1/q:100/plain/images/products/1/3814/859721446/large_Frame__7_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/x0BGYs8ttq4/rs:fit:440:0:1/q:100/plain/images/products/1/3805/859721437/large_Frame__31_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/KDxXqJlEdro/rs:fit:440:0:1/q:100/plain/images/products/1/3803/859721435/large_Frame__29_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/V6mFoHaU_YA/rs:fit:440:0:1/q:100/plain/images/products/1/3807/859721439/large_Frame__33_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/xiRC551rw2U/rs:fit:440:0:1/q:100/plain/images/products/1/3809/859721441/large_Frame__35_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/0umk1f_PUVM/rs:fit:440:0:1/q:100/plain/images/products/1/3808/859721440/large_Frame__34_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/jGGBR3mgka8/rs:fit:440:0:1/q:100/plain/images/products/1/3810/859721442/large_Frame__36_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/BUWsIWJyYbg/rs:fit:440:0:1/q:100/plain/images/products/1/3806/859721438/large_Frame__32_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/c8Ch6qJWp_E/rs:fit:440:0:1/q:100/plain/images/products/1/3816/859721448/large_Frame__51_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/7V6qk4jpvzU/rs:fit:440:0:1/q:100/plain/images/products/1/3813/859721445/large_Frame__50_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/Xy7U5C5ZONo/rs:fit:440:0:1/q:100/plain/images/products/1/3812/859721444/large_Frame__27_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/X7hKll32X2k/rs:fit:440:0:1/q:100/plain/images/products/1/3815/859721447/large_Frame__44_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-			<div className="pages_card">
-				<a href="pages2.html">
-					<Link to="/" >
-						<img src='https://static.insales-cdn.com/r/uGPvecU6ikM/rs:fit:440:0:1/q:100/plain/images/products/1/3817/859721449/large_Frame__22_.png@webp' alt='' className="pages_img" />
-					</Link>
-
-					<h3 className="product_tit">«
-						SEVEN7EEN BODY SILK Лосьон для тела - GREEN JASMINE, 100мл 	</h3>
-					{/* <span className="product_price flex ">4600cом
-					<Link to="/" >
-						<img src='https://icons.veryicon.com/png/o/miscellaneous/unicons/cart-38.png' alt='' className="pages_ic" />
-					</Link>
-					</span> */}
-				</a>
-			</div>
-
-			</div>
-		</>
-	);
+  return (
+    <div className="Product">
+      <div className="hero">
+        <h1>{product.name}</h1>
+        <img src={product.picture} alt={product.name} />
+        <span>{product.price}$</span>
+        <p>{product.description}</p>
+      </div>
+      <div className="info">
+        <p className="info">{product.info}</p>
+      </div>
+    </div>
+  )
 }
-
-export default Product;
