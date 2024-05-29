776cc9bbe3d2604ec71ea3c91ce00614d58d020b
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-29 13:55:53 +0600

    renamed all products

--- src/Components/All/All.js
+++ src/Components/All/All.js
@@ -1,24 +1,24 @@
-import "./AppleProducts.css"
+import "./All.css"
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 import { AddToCart } from "../AddToCart/AddToCart";
-import AddProduct from "../AddProduct/AddProduct";
+import AddProduct from "../../Components/AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
 import Title from "../Title/Title";
 
 
-export default function AllProducts({ sellers }) {
+export default function All({ sellers }) {
 
     const { products } = useContext(AppContext);
-    const output = products.filter(product => product.name && product.appleProducts === true)
+    const output = products.filter(product => product.all === true)
         .map(product => (
             <div key={product.id} className="product">
                 <Link to={"/products/" + product.slug}>
                     <img src={product.picture} alt={product.name} />
                 </Link>
                 <hr />
-                <Link to={"/products/" + product.slug} className="text">
+                <Link to={"/products/" } className="text">
                     {product.name}
                 </Link>
 
@@ -37,7 +37,7 @@ export default function AllProducts({ sellers }) {
     return (
         <>
             <div className="BestSellerContent flex">
-                <Title>All</Title>
+             <div className="tit"><Title>Our all products</Title></div>   
                 <div className="ProductList">
                     <AddProduct sellers={sellers} />
                     {output}
@@ -45,10 +45,4 @@ export default function AllProducts({ sellers }) {
             </div>
         </>
     )
-
-
-
-
-
-
 }
